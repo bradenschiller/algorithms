7d18b25107c4af045cf103193821aaecbd24a595
Braden <braden@DSNs-iMac.local>
2018-08-01 11:44:24 -0500

    first commit

---
+++ javascript/doubleNumber.js
@@ -0,0 +1,12 @@
+// coding challange that takes in an array and doubles each number in the array
+
+let array = [1, 2, 3]
+
+function doubleNumbers(arr) {
+  let newarr = arr.map((number) => {
+      return number * 2
+  })
+  console.log(newarr);
+}
+
+doubleNumbers(array);
