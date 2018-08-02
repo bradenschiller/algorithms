cbee20ade8f8f9af7e773b3c03d7e34969357e0b
bradenschiller17 <bradenschiller17@gmail.com>
2018-08-02 00:17:25 -0500

    added bool to word algorithm

---
+++ javascript/BoolToWord/README.md
@@ -0,0 +1,3 @@
+# Boolean to word 
+
+* Used a ternary statment to see if true or false was given and returned a string accordingly.
---
+++ javascript/BoolToWord/boolToWord.js
@@ -0,0 +1,10 @@
+//Function that takes in a boolean and converts in to string.
+
+function boolToWord(bool) {
+    return (bool ? "Yes" : "No");
+}
+
+boolToWord(true);
+boolToWord(false);
+
+// answer returns true = "Yes", false = "No".
